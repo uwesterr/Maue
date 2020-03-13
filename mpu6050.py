138d806d1cfb6f25d348ad05ef44d66e8d78f633
Uwe Sterr <uwe.sterr@st2c.de>
2020-03-13 15:30:34 +0100

    Update mpu6050.py
    neue version von mark

--- mpu6050.py
+++ mpu6050.py
@@ -5,6 +5,12 @@
 import smbus            #import SMBus module of I2C
 from time import sleep          #import
 import math
+import paho.mqtt.publish as publish
+
+host = "localhost"
+
+
+
 
 #some MPU6050 Registers and their Address
 PWR_MGMT_1   = 0x6B
@@ -23,16 +29,16 @@ GYRO_ZOUT_H  = 0x47
 def MPU_Init():
     #write to sample rate register
     bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
-    
+
     #Write to power management register
     bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
-    
+
     #Write to Configuration register
     bus.write_byte_data(Device_Address, CONFIG, 0)
-    
+
     #Write to Gyro configuration register
     bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
-    
+
     #Write to interrupt enable register
     bus.write_byte_data(Device_Address, INT_ENABLE, 1)
 
@@ -40,10 +46,10 @@ def read_raw_data(addr):
     #Accelero and Gyro value are 16-bit
         high = bus.read_byte_data(Device_Address, addr)
         low = bus.read_byte_data(Device_Address, addr+1)
-    
+
         #concatenate higher and lower value
         value = ((high << 8) | low)
-        
+
         #to get signed value from mpu6050
         if(value > 32768):
                 value = value - 65536
@@ -58,26 +64,40 @@ MPU_Init()
 print (" Reading Data of Gyroscope and Accelerometer")
 
 while True:
-    
+
     #Read Accelerometer raw value
     acc_x = read_raw_data(ACCEL_XOUT_H)
     acc_y = read_raw_data(ACCEL_YOUT_H)
     acc_z = read_raw_data(ACCEL_ZOUT_H)
-    
+
     #Read Gyroscope raw value
     gyro_x = read_raw_data(GYRO_XOUT_H)
     gyro_y = read_raw_data(GYRO_YOUT_H)
     gyro_z = read_raw_data(GYRO_ZOUT_H)
-    
+
     #Full scale range +/- 250 degree/C as per sensitivity scale factor
     Ax = acc_x/16384.0
     Ay = acc_y/16384.0
     Az = acc_z/16384.0
-    
+
     Gx = gyro_x/131.0*180/math.pi
     Gy = gyro_y/131.0*180/math.pi
     Gz = gyro_z/131.0*180/math.pi
-    
 
-    print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az)     
-    sleep(1)
+    #Roll and Pitch (self made)
+    roll = math.atan(acc_y /  math.sqrt(acc_x*acc_x + acc_z*acc_z))
+    # pitch: (nose up/down, about Y axis)
+    pitch = math.atan(acc_x / math.sqrt(acc_y*acc_y + acc_z*acc_z))
+
+    roll_deg = roll * (180.0/math.pi)
+    pitch_deg = pitch * (180.0/math.pi)
+
+    msgs = [{'topic': "IMU/pitch", 'payload': str(pitch_deg)},
+        {'topic': "IMU/roll", 'payload': str(roll_deg)}]
+
+#    print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az)     
+    print ("roll=%.2f" %roll_deg, "tpitch=%.2f" %pitch_deg)
+    # publish.single(topic="IMU/pitch", payload=str(pitch_deg), hostname=host)
+    #mosquitto_pub -h localhost -t "test/message" -m str(pitch_deg)
+    publish.multiple(msgs, hostname=host)
+    sleep(0.5)
